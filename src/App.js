700e0ef91a3e39cc482bf1431ca24e577957d79c
Potato <imrahnfaqiri21@gmail.com>
2023-11-27 12:55:47 -0500

    test page

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <h> hello world</h> <> </>
+      <input type="text"></input>
+      <button> Test </button>
+    </>
+  )
+}
+
+export default App;
